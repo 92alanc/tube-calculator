bfe711777726aba92662b550335d4beeb6eaad6b
Alan Camargo <alan.uk.br@gmail.com>
2022-12-24 19:05:09 +0000

    Implement service

--- feature-search/src/main/kotlin/com/alancamargo/tubecalculator/search/data/model/StationSearchResultsResponse.kt
+++ feature-search/src/main/kotlin/com/alancamargo/tubecalculator/search/data/model/StationSearchResultsResponse.kt
@@ -4,6 +4,6 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-internal data class StationSearchRootResponse(
+internal data class StationSearchResultsResponse(
     @SerialName("matches") val matches: List<StationResponse>?
 )
---
+++ feature-search/src/main/kotlin/com/alancamargo/tubecalculator/search/data/service/SearchService.kt
@@ -0,0 +1,16 @@
+package com.alancamargo.tubecalculator.search.data.service
+
+import com.alancamargo.tubecalculator.search.data.model.StationSearchResultsResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+internal interface SearchService {
+
+    @GET("StopPoint/Search/{query}")
+    suspend fun searchStation(
+        @Path("query") query: String,
+        @Query("modes") modes: String = "dlr,elizabeth-line,national-rail,overground,tube",
+        @Query("oysterOnly") isOysterOnly: Boolean = true
+    ): StationSearchResultsResponse
+}
