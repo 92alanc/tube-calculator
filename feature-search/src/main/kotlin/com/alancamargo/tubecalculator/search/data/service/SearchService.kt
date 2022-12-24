89392b10273da09f82ad4e8f0f6e88acf454d23c
Alan Camargo <alan.uk.br@gmail.com>
2022-12-24 19:08:42 +0000

    Implement result

--- feature-search/src/main/kotlin/com/alancamargo/tubecalculator/search/data/service/SearchService.kt
+++ feature-search/src/main/kotlin/com/alancamargo/tubecalculator/search/data/service/SearchService.kt
@@ -1,6 +1,7 @@
 package com.alancamargo.tubecalculator.search.data.service
 
 import com.alancamargo.tubecalculator.search.data.model.StationSearchResultsResponse
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -12,5 +13,5 @@ internal interface SearchService {
         @Path("query") query: String,
         @Query("modes") modes: String = "dlr,elizabeth-line,national-rail,overground,tube",
         @Query("oysterOnly") isOysterOnly: Boolean = true
-    ): StationSearchResultsResponse
+    ): Response<StationSearchResultsResponse>
 }
