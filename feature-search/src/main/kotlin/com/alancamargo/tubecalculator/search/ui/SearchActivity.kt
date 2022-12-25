da1d2b8b0ec6b8987df822ae73c1a4c935ea5d4a
Alan Camargo <alan.uk.br@gmail.com>
2022-12-25 12:21:25 +0000

    Create activity class and layout

---
+++ feature-search/src/main/kotlin/com/alancamargo/tubecalculator/search/ui/SearchActivity.kt
@@ -0,0 +1,7 @@
+package com.alancamargo.tubecalculator.search.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+internal class SearchActivity : AppCompatActivity()
