5f56df2406c3c9c45c72aae0a8f61339b854fcb3
Alan Camargo <alan.uk.br@gmail.com>
2022-12-25 12:28:33 +0000

    Implementing onCreate

--- feature-search/src/main/kotlin/com/alancamargo/tubecalculator/search/ui/SearchActivity.kt
+++ feature-search/src/main/kotlin/com/alancamargo/tubecalculator/search/ui/SearchActivity.kt
@@ -1,7 +1,24 @@
 package com.alancamargo.tubecalculator.search.ui
 
+import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.alancamargo.tubecalculator.search.databinding.ActivitySearchBinding
+import com.alancamargo.tubecalculator.search.ui.viewmodel.SearchViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-internal class SearchActivity : AppCompatActivity()
+internal class SearchActivity : AppCompatActivity() {
+
+    private var _binding: ActivitySearchBinding? = null
+    private val binding: ActivitySearchBinding
+        get() = _binding!!
+
+    private val viewModel by viewModels<SearchViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
