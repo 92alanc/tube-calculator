766c8798d35b48f9a36f4d322b7b57038d5f8bb0
Alan Camargo <alan.uk.br@gmail.com>
2022-12-25 11:57:27 +0000

    Add error messages

--- feature-search/src/main/kotlin/com/alancamargo/tubecalculator/search/ui/model/UiSearchError.kt
+++ feature-search/src/main/kotlin/com/alancamargo/tubecalculator/search/ui/model/UiSearchError.kt
@@ -2,31 +2,32 @@ package com.alancamargo.tubecalculator.search.ui.model
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import com.alancamargo.tubecalculator.search.R
+import com.alancamargo.tubecalculator.core.design.R as R2
 
 internal enum class UiSearchError(
     @DrawableRes val iconRes: Int,
     @StringRes val messageRes: Int
 ) {
 
-    // TODO
     MISSING_ORIGIN_OR_DESTINATION(
-        iconRes = 0,
-        messageRes = 0
+        iconRes = R2.drawable.ic_underground, // TODO
+        messageRes = R.string.search_error_missing_origin_destination
     ),
     SAME_ORIGIN_AND_DESTINATION(
-        iconRes = 0,
-        messageRes = 0
+        iconRes = R2.drawable.ic_underground, // TODO
+        messageRes = R.string.search_error_same_origin_destination
     ),
     NETWORK(
-        iconRes = 0,
-        messageRes = 0
+        iconRes = R2.drawable.ic_underground, // TODO
+        messageRes = R2.string.message_network_error
     ),
     SERVER(
-        iconRes = 0,
-        messageRes = 0
+        iconRes = R2.drawable.ic_underground, // TODO
+        messageRes = R2.string.message_server_error
     ),
     GENERIC(
-        iconRes = 0,
-        messageRes = 0
+        iconRes = R2.drawable.ic_underground, // TODO
+        messageRes = R2.string.message_generic_error
     )
 }
