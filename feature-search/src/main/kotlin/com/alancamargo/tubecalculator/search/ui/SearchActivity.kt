f52e6381c4eb770da01ea982a66b91fe528b8604
Alan Camargo <alan.uk.br@gmail.com>
2022-12-26 00:03:58 +0000

    Implementing activity

--- feature-search/src/main/kotlin/com/alancamargo/tubecalculator/search/ui/SearchActivity.kt
+++ feature-search/src/main/kotlin/com/alancamargo/tubecalculator/search/ui/SearchActivity.kt
@@ -1,8 +1,12 @@
 package com.alancamargo.tubecalculator.search.ui
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.inputmethod.EditorInfo
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.alancamargo.tubecalculator.search.R
 import com.alancamargo.tubecalculator.search.databinding.ActivitySearchBinding
 import com.alancamargo.tubecalculator.search.ui.viewmodel.SearchViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,5 +24,55 @@ internal class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setUpUi()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_search, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.itemAbout -> {
+                viewModel.onAppInfoClicked()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun setUpUi() {
+        with(binding) {
+            setUpOriginField()
+            setUpDestinationField()
+
+            btUp.setOnClickListener {  }
+            btDown.setOnClickListener {  }
+            btCalculate.setOnClickListener { viewModel.onCalculateClicked() }
+        }
+    }
+
+    private fun ActivitySearchBinding.setUpOriginField() {
+        edtOrigin.setOnEditorActionListener { textField, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                viewModel.searchOrigin(textField.text.toString())
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    private fun ActivitySearchBinding.setUpDestinationField() {
+        edtDestination.setOnEditorActionListener { textField, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                viewModel.searchDestination(textField.text.toString())
+                true
+            } else {
+                false
+            }
+        }
     }
 }
