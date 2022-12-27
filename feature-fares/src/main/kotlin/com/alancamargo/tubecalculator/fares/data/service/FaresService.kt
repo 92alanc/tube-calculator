c451762c70022ec476d8e74f12ccc95009f02eb6
Alan Camargo <alan.uk.br@gmail.com>
2022-12-27 15:54:38 +0000

    Implement FaresService

---
+++ feature-fares/src/main/kotlin/com/alancamargo/tubecalculator/fares/data/service/FaresService.kt
@@ -0,0 +1,15 @@
+package com.alancamargo.tubecalculator.fares.data.service
+
+import com.alancamargo.tubecalculator.fares.data.model.FareRootResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+internal interface FaresService {
+
+    @GET("StopPoint/{origin}/FareTo/{destination}")
+    suspend fun getFares(
+        @Path("origin") originId: String,
+        @Path("destination") destinationId: String
+    ): Response<FareRootResponse>
+}
