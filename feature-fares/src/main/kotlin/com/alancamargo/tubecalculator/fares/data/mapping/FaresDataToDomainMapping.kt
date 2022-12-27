a6f207188acf18b0758743aa65476dcff4d9eb34
Alan Camargo <alan.uk.br@gmail.com>
2022-12-27 16:07:00 +0000

    Implement data to domain mapping

---
+++ feature-fares/src/main/kotlin/com/alancamargo/tubecalculator/fares/data/mapping/FaresDataToDomainMapping.kt
@@ -0,0 +1,36 @@
+package com.alancamargo.tubecalculator.fares.data.mapping
+
+import com.alancamargo.tubecalculator.fares.data.model.FareListRootResponse
+import com.alancamargo.tubecalculator.fares.data.model.FareResponse
+import com.alancamargo.tubecalculator.fares.data.model.TicketResponse
+import com.alancamargo.tubecalculator.fares.data.model.TicketTimeResponse
+import com.alancamargo.tubecalculator.fares.domain.model.Fare
+import com.alancamargo.tubecalculator.fares.domain.model.FareListRoot
+import com.alancamargo.tubecalculator.fares.domain.model.Ticket
+import com.alancamargo.tubecalculator.fares.domain.model.TicketTime
+
+internal fun FareListRootResponse.toDomain() = FareListRoot(
+    header = header,
+    fares = fares.map { it.toDomain() }
+)
+
+private fun FareResponse.toDomain() = Fare(
+    label = label,
+    origin = origin,
+    destination = destination,
+    description = description,
+    passengerType = passengerType,
+    tickets = tickets.map { it.toDomain() },
+    messages = messages.map { it.message }
+)
+
+private fun TicketResponse.toDomain() = Ticket(
+    type = type.label,
+    time = time.toDomain(),
+    cost = cost
+)
+
+private fun TicketTimeResponse.toDomain() = TicketTime(
+    label = label,
+    description = description
+)
