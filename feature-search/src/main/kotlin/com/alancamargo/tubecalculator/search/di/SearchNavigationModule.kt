9e646e89a5146fca1e54ddd2c011954859808b9c
Alan Camargo <alan.uk.br@gmail.com>
2022-12-28 15:15:05 +0000

    Implement shimmer

--- feature-search/src/main/kotlin/com/alancamargo/tubecalculator/search/di/SearchNavigationModule.kt
+++ feature-search/src/main/kotlin/com/alancamargo/tubecalculator/search/di/SearchNavigationModule.kt
@@ -1,38 +1,20 @@
 package com.alancamargo.tubecalculator.search.di
 
-import android.content.Context
-import com.alancamargo.tubecalculator.common.ui.model.UiStation
-import com.alancamargo.tubecalculator.navigation.FaresActivityNavigation
 import com.alancamargo.tubecalculator.navigation.SearchActivityNavigation
 import com.alancamargo.tubecalculator.search.ui.navigation.SearchActivityNavigationImpl
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.scopes.ActivityScoped
 
 @Module
 @InstallIn(ActivityComponent::class)
-internal object SearchNavigationModule {
+internal abstract class SearchNavigationModule {
 
-    @Provides
+    @Binds
     @ActivityScoped
-    fun provideSearchActivityNavigation(
+    abstract fun bindSearchActivityNavigation(
         impl: SearchActivityNavigationImpl
-    ): SearchActivityNavigation = impl
-
-    @Provides
-    @ActivityScoped
-    fun provideFaresActivityNavigation(): FaresActivityNavigation {
-        return object : FaresActivityNavigation {
-            override fun startActivity(
-                context: Context,
-                origin: UiStation,
-                destination: UiStation,
-                busAndTramJourneyCount: Int
-            ) {
-                // TODO: remove when real implementation is ready
-            }
-        }
-    }
+    ): SearchActivityNavigation
 }
