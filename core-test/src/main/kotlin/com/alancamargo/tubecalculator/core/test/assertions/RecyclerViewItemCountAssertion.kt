1bd87e799f2e2cdfb66f14c45cce63f40ad03ed6
Alan Camargo <alan.uk.br@gmail.com>
2023-02-09 00:36:22 +0000

    Implement view assertion

---
+++ core-test/src/main/kotlin/com/alancamargo/tubecalculator/core/test/assertions/RecyclerViewItemCountAssertion.kt
@@ -0,0 +1,23 @@
+package com.alancamargo.tubecalculator.core.test.assertions
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.NoMatchingViewException
+import androidx.test.espresso.ViewAssertion
+import com.google.common.truth.Truth.assertThat
+
+fun withRecyclerViewItemCount(expectedCount: Int): RecyclerViewItemCountAssertion {
+    return RecyclerViewItemCountAssertion(expectedCount)
+}
+
+class RecyclerViewItemCountAssertion(private val expectedCount: Int) : ViewAssertion {
+
+    override fun check(view: View?, noViewFoundException: NoMatchingViewException?) {
+        if (noViewFoundException != null) {
+            throw noViewFoundException
+        }
+
+        val recyclerView = view as RecyclerView
+        assertThat(recyclerView.adapter?.itemCount).isEqualTo(expectedCount)
+    }
+}
