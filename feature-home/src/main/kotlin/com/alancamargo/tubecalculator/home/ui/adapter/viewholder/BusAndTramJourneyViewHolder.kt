b5a7040154af145421bb10f5fcabea8f1450bdf4
Alan Camargo <alan.uk.br@gmail.com>
2023-02-11 18:35:32 +0000

    Implement adapter

--- feature-home/src/main/kotlin/com/alancamargo/tubecalculator/home/ui/adapter/viewholder/BusAndTramJourneyViewHolder.kt
+++ feature-home/src/main/kotlin/com/alancamargo/tubecalculator/home/ui/adapter/viewholder/BusAndTramJourneyViewHolder.kt
@@ -1,20 +1,22 @@
-package com.alancamargo.tubecalculator.home.ui.adapter
+package com.alancamargo.tubecalculator.home.ui.adapter.viewholder
 
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.alancamargo.tubecalculator.common.ui.model.Journey
 import com.alancamargo.tubecalculator.home.R
 import com.alancamargo.tubecalculator.home.databinding.ItemBusTramJourneyBinding
 
 internal class BusAndTramJourneyViewHolder(
     private val binding: ItemBusTramJourneyBinding,
-    private val onItemClick: (Journey) -> Unit
-) : ViewHolder(binding.root) {
+    onItemClick: (Journey) -> Unit
+) : JourneyViewHolder(binding.root, onItemClick) {
 
-    fun bindTo(journey: Journey.BusAndTram) = with(binding) {
+    override fun bindTo(journey: Journey) = with(binding) {
+        require(journey is Journey.BusAndTram)
+
         txtBusAndTramJourneyCount.text = root.context.getString(
             R.string.number_of_journeys_format,
             journey.journeyCount
         )
+
         root.setOnClickListener { onItemClick(journey) }
     }
 }
--- feature-home/src/main/kotlin/com/alancamargo/tubecalculator/home/ui/adapter/viewholder/RailJourneyViewHolder.kt
+++ feature-home/src/main/kotlin/com/alancamargo/tubecalculator/home/ui/adapter/viewholder/RailJourneyViewHolder.kt
@@ -1,15 +1,16 @@
-package com.alancamargo.tubecalculator.home.ui.adapter
+package com.alancamargo.tubecalculator.home.ui.adapter.viewholder
 
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.alancamargo.tubecalculator.common.ui.model.Journey
 import com.alancamargo.tubecalculator.home.databinding.ItemRailJourneyBinding
 
 internal class RailJourneyViewHolder(
     private val binding: ItemRailJourneyBinding,
-    private val onItemClick: (Journey) -> Unit
-) : ViewHolder(binding.root) {
+    onItemClick: (Journey) -> Unit
+) : JourneyViewHolder(binding.root, onItemClick) {
 
-    fun bindTo(journey: Journey.Rail) = with(binding) {
+    override fun bindTo(journey: Journey) = with(binding) {
+        require(journey is Journey.Rail)
+
         txtOrigin.text = journey.origin.name
         txtDestination.text = journey.destination.name
         root.setOnClickListener { onItemClick(journey) }
