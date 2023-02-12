b5b1b115d6569f71bf6e3a9f689c44698413dcbc
Alan Camargo <alan.uk.br@gmail.com>
2023-02-12 16:00:52 +0000

    Update diff callback

--- feature-home/src/main/kotlin/com/alancamargo/tubecalculator/home/ui/adapter/JourneyDiffCallback.kt
+++ feature-home/src/main/kotlin/com/alancamargo/tubecalculator/home/ui/adapter/JourneyDiffCallback.kt
@@ -6,24 +6,22 @@ import com.alancamargo.tubecalculator.common.ui.model.Journey
 internal object JourneyDiffCallback : DiffUtil.ItemCallback<Journey>() {
 
     override fun areItemsTheSame(oldItem: Journey, newItem: Journey): Boolean {
-        val rail = oldItem is Journey.Rail && newItem is Journey.Rail
-        val busAndTram = oldItem is Journey.BusAndTram && newItem is Journey.BusAndTram
+        val bothRail = oldItem is Journey.Rail && newItem is Journey.Rail
+        val bothBusAndTram = oldItem is Journey.BusAndTram && newItem is Journey.BusAndTram
 
-        return if (rail) {
-            require(oldItem is Journey.Rail && newItem is Journey.Rail)
+        return bothRail || bothBusAndTram
+    }
+
+    override fun areContentsTheSame(oldItem: Journey, newItem: Journey): Boolean {
+        return if (oldItem is Journey.Rail && newItem is Journey.Rail) {
             val sameOrigin = oldItem.origin == newItem.origin
             val sameDestination = oldItem.destination == newItem.destination
 
             sameOrigin && sameDestination
-        } else if (busAndTram) {
-            require(oldItem is Journey.BusAndTram && newItem is Journey.BusAndTram)
+        } else if (oldItem is Journey.BusAndTram && newItem is Journey.BusAndTram) {
             oldItem.journeyCount == newItem.journeyCount
         } else {
             false
         }
-    }
-
-    override fun areContentsTheSame(oldItem: Journey, newItem: Journey): Boolean {
-        return areItemsTheSame(oldItem, newItem)
     }
 }
